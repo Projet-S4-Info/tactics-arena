2ab729e77357c2456946733411e0c4f621620bf7
robinbutel <robinbutel@hotmail.com>
2020-03-19 11:27:12 +0100

    update to errt

--- src/errt.h
+++ src/errt.h
@@ -3,8 +3,6 @@
 #include "common.h"
 #define NUM_ERROR 4
 
-typedef enum error_type {OK, POINTER_NULL, SDL_ERROR, COORD_OUT_OF_BOUND} err_t;
-
 char error_message[NUM_ERROR][STR_SHORT] = {"OK\n", "ERROR: Pointer is Null\n", "ERROR : SDL\n", "ERROR : Coordinates out of limits\n"};
 
 #endif
