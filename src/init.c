1538ed0800c0ba832074b75e04a2ec2b5035b755
robinbutel <robinbutel@hotmail.com>
2020-04-16 15:46:52 +0200

    :bug: coord init bugfix

--- src/init.c
+++ src/init.c
@@ -22,8 +22,6 @@ err_t init_spawn(Entity * e, Coord c)
     if(verbose)printf("Setting %s Spawn at %d %d\n", e->cha_name, c.x, c.y);
 
     free_spawn(e);
-
-    if(verbose)printf("%s Spawn Set to %d %d\n", e->cha_name, e->coords.x, e->coords.y);
     
     return OK;
 }
@@ -126,17 +124,18 @@ err_t ent_init_test(Entity *e, char title[STR_SHORT])
             (e+i)->cha_id = i+1;
             (e+i)->direction = W;
             init_spawn(e+i, ally_spawn[s]);
+            if(verbose)printf("%s Spawn Set to %d %d\n", e->cha_name, e->coords.x, e->coords.y);
+
         }
         else
         {
             (e+i)->cha_id = (i+1)*-1;
             (e+i)->direction = S;
             init_spawn(e+i, foe_spawn[s]);
+            if(verbose)printf("%s Spawn Set to %d %d\n", e->cha_name, e->coords.x, e->coords.y);
         }
 
         (e+i)->active = Alive;
-        (e+i)->coords.x = 0;
-        (e+i)->coords.y = 0;
         (e+i)->act_points = 3;
 
         for(j=0;j<NUM_STATS;j++)
