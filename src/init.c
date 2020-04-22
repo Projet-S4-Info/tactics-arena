8e378d41d75fb2790da17a72b378306ac9318dc8
ThibaultDct <thibault.doucet44@gmail.com>
2020-04-22 15:47:18 +0200

    :bug: Bug fix

--- src/init.c
+++ src/init.c
@@ -158,10 +158,10 @@ err_t ent_init_test(Entity *e, char title[STR_SHORT])
     (e + Mage)->cha_class->cla_abilities = &mage_ab[rand()%3][0];
 
     //DEAD CHARACTER FOR TESTS
-    int d = rand()%(NUM_CLASS-1);
+    /*int d = rand()%NUM_CLASS;
     (e+d)->active = Dead;
     Tile * t = getTile((e+d)->coords);
-    t->entity = NULL;
+    t->entity = NULL;*/
 
     return OK;
 }
