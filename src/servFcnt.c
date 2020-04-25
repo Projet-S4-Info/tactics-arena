f92b6b84be3e1848a3f57e53a0a55d965444a329
robinbutel <robinbutel@hotmail.com>
2020-04-25 19:21:44 +0200

    :bug: readding sleep

--- src/servFcnt.c
+++ src/servFcnt.c
@@ -116,6 +116,7 @@ void * simple_recep(void *container, int size, int socket)
 {
   while (TRUE)
   {
+    sleep(2);
     if(verbose >= 4)printf("Waiting in simple recep \n");
     if (recv(socket, container, size, MSG_DONTWAIT) > -1)
     {
